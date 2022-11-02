db6ec2eceb27861e194bfb1a1633dfd77beb0386
Muhammed Saeed <Stringsaeed@gmail.com>
2022-11-02 11:48:49 +0200

    feat(android): adding open app settings function

--- android/src/main/java/com/reactnativenotificationsutils/NotificationsUtilsModule.java
+++ android/src/main/java/com/reactnativenotificationsutils/NotificationsUtilsModule.java
@@ -1,8 +1,16 @@
 package com.reactnativenotificationsutils;
 
-import androidx.annotation.NonNull;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
-import com.facebook.react.bridge.Promise;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -10,24 +18,43 @@ import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = NotificationsUtilsModule.NAME)
 public class NotificationsUtilsModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "NotificationsUtils";
+  public static final String NAME = "NotificationsUtils";
 
-    public NotificationsUtilsModule(ReactApplicationContext reactContext) {
-        super(reactContext);
+  public NotificationsUtilsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void openAppNotificationsSettings(String channelId) {
+    final Activity activity = getCurrentActivity();
+    final Context context = getReactApplicationContext().getApplicationContext();
+
+    if (activity == null) {
+      return;
     }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
+    Intent intent;
+    if (Build.VERSION.SDK_INT >= 26) {
+      if (channelId != null) {
+        intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+        intent.putExtra(Settings.EXTRA_CHANNEL_ID, channelId);
+      } else {
+        intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+      }
+      intent.putExtra(Settings.EXTRA_APP_PACKAGE, context.getPackageName());
+    } else {
+      intent = new Intent(Settings.ACTION_APPLICATION_SETTINGS);
     }
 
+    intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void multiply(double a, double b, Promise promise) {
-        promise.resolve(a * b);
-    }
+    activity.runOnUiThread(() -> context.startActivity(intent));
+  }
 
 }
